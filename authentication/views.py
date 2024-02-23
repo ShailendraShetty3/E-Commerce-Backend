c55a68aaa5f5264147891aa6a8ef6ae25af71f18
Shailendra Shetty <shailendra.shetty@AMIAND.LOCAL>
2024-02-23 16:10:43 +0530

    change password added

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
+from .serializers import UserChangePasswordSerializer
 
 from .renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -40,8 +41,8 @@ class UserLoginView(APIView):
             if user is not None:
                 return Response({'token':token,'msg':'Login Success'}, status=status.HTTP_200_OK)
             else:
-                return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_200_OK)
-
+                return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserProfileView(APIView):
     renderer_classes = [UserRenderer]
@@ -52,3 +53,12 @@ class UserProfileView(APIView):
     
 
     
+class UserChangePasswordView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(data=request.data,
+        context={'user':request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
